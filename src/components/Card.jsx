175b1342940e26c93ccfd0b765b519ffad2e61a2
Syed Ahtsham Ul Hassan <syed.hassan@uni-potsdam.de>
2024-08-11 23:47:59 -0400

    Create the Card compnent and add into the Homecards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
-const Card = () => {
-  return (
-    
-  )
+const Card = ({ bg = 'bg-gray-100', title, subtitle, btnText, btnColor = "bg-black", btnHoverColor ="hover:bg-gray-700"}) => {
+    return (
+        <>
+            <div className={`${bg} p-6 rounded-lg shadow-md `}>
+                <h2 className="text-2xl font-bold">{title}</h2>
+                <p className="mt-2 mb-4">
+                    {subtitle}
+                </p>
+                <a
+                    href="/jobs.html"
+                    className={`inline-block ${btnColor} text-white rounded-lg px-4 py-2 ${btnHoverColor}`}
+                >
+                    {btnText}
+                </a>
+            </div>
+        </>
+    )
 }
 
 export default Card
