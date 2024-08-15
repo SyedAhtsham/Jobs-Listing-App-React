bccba5d13a60a4df8f33be9f4960e213b76592e8
Syed Ahtsham Ul Hassan <syed.hassan@uni-potsdam.de>
2024-08-14 20:47:21 -0400

    Create a loader to get a single job for an id

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -32,23 +32,32 @@
 
 
 
-
 // export default JobPage
 
+import { useParams, useLoaderData } from 'react-router-dom'
+import Spinner from '../components/Spinner'
 
 import React from 'react'
 
 const JobPage = () => {
+  const { id } = useParams();
+  const job = useLoaderData();
+
   return (
-    <div>JobPage</div>
+    <>
+      {/* {console.log(job)} */}
+      <h1>{job.title}</h1>
+    </>
   )
 }
 
 
 const jobLoader = async ({ params }) => {
-    const res = await fetch(`/api/jobs/${params.id}`)
-    const data = res.json()
-    return data
+  const res = await fetch(`/api/jobs/${params.id}`)
+  
+  const data = await res.json()
+  
+  return data;
 }
 
 export {JobPage as default, jobLoader}
